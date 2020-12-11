d94ff7ae8f880716dc0f57e767c2cd41a8abf269
Haloren <mckennakyler@gmail.com>
2020-12-11 01:22:10 -0700

    seed file will destroy first on run & added user dependent in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :locations
-    has_many :notifications
+    has_many :locations, dependent: :destroy
+    has_many :notifications, dependent: :destroy
 
     validates :email, presence: true, uniqueness: true
 end
